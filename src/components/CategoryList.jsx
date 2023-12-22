5077125cb1ebdac07f603c5684518607daabe7f1
Daylan <102883290+daylanarias1@users.noreply.github.com>
2023-12-22 12:49:39 -0600

    wq

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -12,41 +12,33 @@ const CategoryList = ({ categoria, videos }) => {
 
   const filteredVideos = videos.filter(video => video.codigoCategoria === categoria.codigo);
 
- 
+
   const scrollVideos = (direction) => {
     let scrollPosition = videosWrapper.current.scrollLeft;
-     // Considera el ancho del elemento y su margen
+    // Considera el ancho del elemento y su margen
     scrollPosition += direction * itemWidth;
- 
+
     videosWrapper.current.scrollTo({
       left: scrollPosition,
       behavior: 'smooth' // Agrega el comportamiento suave
     });
- 
+
     let isAtRightLimit = scrollPosition + videosWrapper.current.clientWidth >= videosWrapper.current.scrollWidth;
     let isAtLeftLimit = scrollPosition <= 0;
- 
+
     prevButton.current.style.visibility = isAtLeftLimit ? 'hidden' : 'visible';
     nextButton.current.style.visibility = isAtRightLimit ? 'hidden' : 'visible';
   }
- 
+
   useEffect(() => {
     prevButton.current.style.visibility = 'hidden';
-
-    let scrollPosition = videosWrapper.current.scrollLeft;
-
-    scrollPosition += 1 * itemWidth;
-
-    let isAtRightLimit = scrollPosition + videosWrapper.current.clientWidth >= videosWrapper.current.scrollWidth;
-
-    nextButton.current.style.visibility = isAtRightLimit ? 'hidden' : 'visible';
   }, []);
- 
+
   return (
-    
+
     <div className="category-container">
       <div className="category-header">
-        <h1 className="category-tile"  style={{ backgroundColor: categoria.color }}>{categoria.nombre}</h1>
+        <h1 className="category-tile" style={{ backgroundColor: categoria.color }}>{categoria.nombre}</h1>
         <p>{categoria.descripcion}</p>
       </div>
       <div className="carousel-container">
@@ -54,9 +46,13 @@ const CategoryList = ({ categoria, videos }) => {
           {'<'}
         </button>
         <div className="video-container" ref={videosWrapper}>
-          {filteredVideos.map((video) => (
-            <Video key={video.id} video={video} />
-          ))}
+          {filteredVideos.length > 0 ? (
+            filteredVideos.map((video) => (
+              <Video key={video.id} video={video} />
+            ))
+          ) : (
+            <h1 className="category-tile">Sin videos</h1>
+          )}
         </div>
         <button className="carousel-button next-button" onClick={() => scrollVideos(1)} ref={nextButton}>
           {'>'}
@@ -64,5 +60,5 @@ const CategoryList = ({ categoria, videos }) => {
       </div>
     </div>
   );
- };
+};
 export default CategoryList;
